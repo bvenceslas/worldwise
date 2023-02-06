c8e6d4d605e9ffadc165c9fe44dda83249a47435
bvenceslas <bvenceslas@gmail.com>
2023-02-06 12:30:12 +0200

    refactor: Change site font

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Our App Here
+      React App
+      <p>This is a showcase of font changing</p>
     </div>
   );
 }
