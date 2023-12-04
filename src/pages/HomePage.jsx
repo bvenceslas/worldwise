7c63d5b1e38e46dedda361bf4733f499aa2d34f1
bvenceslas <bvenceslas@gmail.com>
2023-12-04 09:09:09 +0200

    starter files

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PageNav from "../components/PageNav";
+
+export default function HomePage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+    </div>
+  );
+}
