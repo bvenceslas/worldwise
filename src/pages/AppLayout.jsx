3236bc9779e0abae5bc08b60b4975357375aaf25
bvenceslas <bvenceslas@gmail.com>
2023-12-18 09:09:09 +0200

    add app layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,10 @@
-import AppNav from "../components/AppNav";
 import styles from "./AppLayout.module.css";
+import Sidebar from "../components/Sidebar";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
-      <AppNav />
-      <h1>APP</h1>
+      <Sidebar />
     </div>
   );
 }
