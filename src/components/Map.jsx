4508e342606c56981bb3c155d03a07634e7ac9d4
bvenceslas <bvenceslas@gmail.com>
2024-02-01 09:09:09 +0200

    load cities on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
@@ -11,21 +16,28 @@ export default function Map() {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>MAP</h1>
-      <h2>
-        Position: {lat} , {lng}
-      </h2>
-      <button
-        onClick={() => {
-          setSearchParams({
-            lat: 32,
-            lng: 60,
-          });
-        }}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.mapContainer}
       >
-        Change Position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
